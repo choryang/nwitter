679b9c1ad3ec0fb2ab826326599324da16ee849d
choryang <okee0815@gmail.com>
2021-03-01 18:11:24 +0900

    nweeting

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter =  () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    return (
-        <Router>
-            <Switch>
-                {isLoggedIn ? 
-                <>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                </> : 
-                <Route exact path="/">
-                    <Auth />
-                </Route>}
-            </Switch>
-        </Router>
-    );
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj}/>
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default AppRouter;
